8f23937b873e0da174378874350c75bcd1c30db4
rsgc-murphy-o <omurphy@rsgc.on.ca>
2017-04-20 14:40:02 -0400

    blocks are falling at random intervals and now can be detected and hit by the ball controlled by the user

--- Maze/GameScene.swift
+++ Maze/GameScene.swift
@@ -21,17 +21,24 @@ class GameScene: SKScene {
         Ball.zPosition = 1 
         addChild(Ball)
         
-        Block.position = CGPoint(x: size.width/2, y: size.height/2)
-        Block.zPosition = 1
-        addChild(Block)
-        
     let background = SKSpriteNode(imageNamed: "Icy Background")
         background.position = CGPoint(x: size.width/2, y: size.height/2)
         background.size = self.frame.size
         
         background.zPosition = -1
         addChild(background)
-
+        
+        let actionWait = SKAction.wait(forDuration: 2)
+        let actionSpawn = SKAction.run () {[weak self] in self?.spawnObstacles()}
+        let actionSequence = SKAction.sequence ([actionWait, actionSpawn])
+        let actionObstacleRepeat = SKAction.repeatForever(actionSequence)
+        run(actionObstacleRepeat)
+        
+    }
+    
+    override func update(_ currentTime: TimeInterval){
+        // check for collisions
+        checkCollisions()
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -75,4 +82,69 @@ class GameScene: SKScene {
         Ball.run(actionMove)
         
     }
+    
+    func spawnObstacles(){
+    
+        let obstacle = SKSpriteNode(imageNamed: "Block")
+        
+        obstacle.setScale(6)
+        
+        let horizontalPosition = CGFloat(arc4random_uniform(UInt32(size.width)))
+        let verticalPosition = size.height + obstacle.size.height
+        
+        let startingPosition = CGPoint(x: horizontalPosition, y: verticalPosition)
+        
+        obstacle.position = startingPosition
+        obstacle.zPosition = 2
+        
+        obstacle.name = "obstacle"
+        
+        addChild(obstacle)
+        
+        
+        
+        let endingPosition = CGPoint(x: horizontalPosition, y: 0 - obstacle.size.height)
+        
+        let actionMove = SKAction.move(to: endingPosition, duration: 5)
+        
+        let actionRemove = SKAction.removeFromParent()
+        
+        let actionSequene = SKAction.sequence([actionMove, actionRemove])
+        
+        obstacle.run(actionSequene)
+        
+    
+    }
+    
+    func checkCollisions() {
+    
+        var hitObstacles : [SKSpriteNode] = []
+        
+        enumerateChildNodes(withName: "obstacle", using: {
+        
+            node, _ in
+            
+            let obstacle = node as! SKSpriteNode
+            
+            if obstacle.frame.insetBy(dx: 20, dy: 20).intersects(self.Ball.frame.insetBy(dx: 40, dy: 40)) {
+                
+                hitObstacles.append(obstacle)
+            }
+        
+        })
+        
+        for obstacle in hitObstacles {
+            ballHit(by: obstacle)
+        }
+        
+    }
+    
+    func ballHit (by obstacle: SKSpriteNode) {
+        obstacle.removeFromParent()
+    }
+    
 }
+
+
+
+
