756008385e58ca7eed81f0e5f3db9686dedbc9e7
rsgc-murphy-o <omurphy@rsgc.on.ca>
2017-04-24 09:28:53 -0400

    almost finished comments

--- Maze/GameScene.swift
+++ Maze/GameScene.swift
@@ -5,7 +5,6 @@
 //  Created by Oliver Murphy on 2017-03-02.
 //  Copyright © 2017 Oliver Murphy. All rights reserved.
 //
-
 import SpriteKit
 
 class GameScene: SKScene {
@@ -15,7 +14,7 @@ class GameScene: SKScene {
     let Block = SKSpriteNode(imageNamed: "Block")                              // Add the block sprite.
     
     override func didMove( to view: SKView) {
-        backgroundColor = SKColor.black
+        backgroundColor = SKColor.black                                        // Background color of the scene is solid black.
         Ball.position = CGPoint(x: size.width/2, y: size.height/2)             // Configure how the ball sprite looks.
         Ball.zPosition = 1                                                     // Ensures that the ball is drawn above the background.
         addChild(Ball)                                                         // Add the ball sprite to the scene.
@@ -26,11 +25,11 @@ class GameScene: SKScene {
         background.zPosition = -1                                              // Ensures that the background is drawn under the ball sprite.
         addChild(background)                                                   // Add the background sprite to the scene.
         
-        let actionWait = SKAction.wait(forDuration: 2)
-        let actionSpawn = SKAction.run () {[weak self] in self?.spawnObstacles()}
-        let actionSequence = SKAction.sequence ([actionWait, actionSpawn])
-        let actionObstacleRepeat = SKAction.repeatForever(actionSequence)
-        run(actionObstacleRepeat)
+        let actionWait = SKAction.wait(forDuration: 2)                         // Periodically spawn obstacles.
+        let actionSpawn = SKAction.run () {[weak self] in self?.spawnObstacles()}// Obstacles spawn.
+        let actionSequence = SKAction.sequence ([actionWait, actionSpawn])     // The sequence where the blocks spawn.
+        let actionObstacleRepeat = SKAction.repeatForever(actionSequence)      // Configures action sequence to repeat forever.
+        run(actionObstacleRepeat)                                              // Now run start the sequence.
         
     }
     
@@ -55,7 +54,7 @@ class GameScene: SKScene {
             return
         }
         
-        let touchLocation = touch.location(in: self)
+        let touchLocation = touch.location(in: self)                           // Get the location of the touch.
         print(touchLocation.x)                                                 // Print the x location in the console.
         print(touchLocation.y)                                                 // Print the y location in the console.
         moveBall(touchLocation: touchLocation)                                 // Use the moveBall method again.
@@ -71,67 +70,39 @@ class GameScene: SKScene {
     }
     
     func spawnObstacles(){                                                     // Method to spawn multiple obstacles for the ball to dodge.
-    
-        let obstacle = SKSpriteNode(imageNamed: "Block")
+        let obstacle = SKSpriteNode(imageNamed: "Block")                       // Create instance of the obstacle.
+        obstacle.setScale(6)                                                   // Set the size of the obstacle.
+        let horizontalPosition = CGFloat(arc4random_uniform(UInt32(size.width)))// Define the starting position of the obstacle. (random)
+        let verticalPosition = size.height + obstacle.size.height              // Make sure obstacle is starting off screen.
+        let startingPosition = CGPoint(x: horizontalPosition, y: verticalPosition)// Define the starting position for the obstacle.
+        obstacle.position = startingPosition                                   // Set the starting position.
+        obstacle.zPosition = 2                                                 // Ensure the block sprite spawns above the background and ball.
+        obstacle.name = "obstacle"                                             // Define the name of the obstacle.
+        addChild(obstacle)                                                     // Add the obstacle sprite to the scene.
         
-        obstacle.setScale(6)
-        
-        let horizontalPosition = CGFloat(arc4random_uniform(UInt32(size.width)))
-        let verticalPosition = size.height + obstacle.size.height
-        
-        let startingPosition = CGPoint(x: horizontalPosition, y: verticalPosition)
-        
-        obstacle.position = startingPosition
-        obstacle.zPosition = 2
-        
-        obstacle.name = "obstacle"
-        
-        addChild(obstacle)
-        
-        
-        
+        // Move the obstacle.
         let endingPosition = CGPoint(x: horizontalPosition, y: 0 - obstacle.size.height)
-        
-        let actionMove = SKAction.move(to: endingPosition, duration: 5)
-        
-        let actionRemove = SKAction.removeFromParent()
-        
-        let actionSequene = SKAction.sequence([actionMove, actionRemove])
-        
-        obstacle.run(actionSequene)
-        
+        let actionMove = SKAction.move(to: endingPosition, duration: 5)        // Obstacle moves to end position defined<.
+        let actionRemove = SKAction.removeFromParent()                         // This will remove the obstacle from the scene.
+        let actionSequene = SKAction.sequence([actionMove, actionRemove])      // Tells what actions to run and in which order.
+        obstacle.run(actionSequene)                                            // instead of running a single action, it runs the sequence.
     
     }
     
     func checkCollisions() {
-    
         var hitObstacles : [SKSpriteNode] = []
-        
         enumerateChildNodes(withName: "obstacle", using: {
-        
             node, _ in
-            
             let obstacle = node as! SKSpriteNode
-            
             if obstacle.frame.insetBy(dx: 20, dy: 20).intersects(self.Ball.frame.insetBy(dx: 40, dy: 40)) {
-                
                 hitObstacles.append(obstacle)
             }
-        
         })
-        
         for obstacle in hitObstacles {
             ballHit(by: obstacle)
         }
-        
     }
-    
     func ballHit (by obstacle: SKSpriteNode) {
         obstacle.removeFromParent()
     }
-    
 }
-
-
-
-
